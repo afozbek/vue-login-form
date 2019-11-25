8eb8cdc1185199f62febb33f5c7a5d919fdc6cf2
Furkan Ozbek <fozbek@boynergrup.com>
2019-11-25 16:35:21 +0300

    initial form setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="m-flexContainer">
+      <img
+        class="a-heroImage"
+        alt="Vue logo"
+        src="https://colorlib.com/etc/lf/Login_v18/images/bg-01.jpg"
+      />
+      <div class="m-formContainer">
+        <LoginForm />
+      </div>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+import LoginForm from "./components/LoginForm.vue";
+
+export default {
+  name: "app",
+  components: {
+    LoginForm
+  }
+};
+</script>
+
+<style lang="scss">
+/* GLOBAL SCSS IMPORT */
+@import "./scss/main.scss";
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form class="m-loginForm">
+    <h1 class="m-loginForm__desc">Login to Continue</h1>
+
+    <input class="m-loginForm__input" placeholder="Email" name="email" type="text" />
+
+    <input class="m-loginForm__input" type="password" placeholder="Password" name="password" />
+
+    <a class="m-loginForm__forgotBtn" href="#">Forgot Password?</a>
+
+    <button class="m-loginForm__submitBtn">Login</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
