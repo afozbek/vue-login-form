609edc8c891bc08e81c0711e2fb9add72a5468ae
Furkan Ozbek <fozbek@boynergrup.com>
2019-11-26 12:59:10 +0300

    login formu güncellendi

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,10 +1,21 @@
 <template>
   <form class="m-loginForm">
     <h1 class="m-loginForm__desc">{{header}}</h1>
+    <div class="m-loginForm__inputGroup">
+      <input class="m-loginForm__input" v-model="email" placeholder=" " name="email" type="text" />
+      <div class="m-loginForm__placeholder">Email</div>
+    </div>
 
-    <input class="m-loginForm__input" placeholder="Email" name="email" type="text" />
-
-    <input class="m-loginForm__input" type="password" placeholder="Password" name="password" />
+    <div class="m-loginForm__inputGroup">
+      <input
+        class="m-loginForm__input"
+        v-model="password"
+        placeholder=" "
+        type="password"
+        name="password"
+      />
+      <div class="m-loginForm__placeholder">Password</div>
+    </div>
 
     <a class="m-loginForm__forgotBtn" href="#">Forgot Password?</a>
 
@@ -14,6 +25,13 @@
 
 <script>
 export default {
-  props: ["header"]
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  props: ["header"],
+  methods: {}
 };
 </script>
